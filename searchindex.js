b6b75fde8e9013c34691870a7fb998cf7814b812
Ennis Massey <ennisbaradine@gmail.com>
2016-04-08 21:41:56 +1200

    FIXED

--- searchindex.js
+++ searchindex.js
@@ -1,1 +1,1 @@
-../V4/build/searchindex.js
+Search.setIndex({envversion:48,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to Wet Ferret Engine Docs&#8217;s documentation!"],titleterms:{doc:0,document:0,engin:0,ferret:0,indic:0,tabl:0,welcom:0,wet:0}})
